83f33ad1b6be16539b68aac869729ee00f6b4a4b
Mohammad Saify <Mohammad.Saify@glu.com>
2021-01-13 17:37:02 +0530

    API call from SDK using a UILable .

--- HelloJunaidSDKSample/ViewController.swift
+++ HelloJunaidSDKSample/ViewController.swift
@@ -7,13 +7,24 @@
 //
 
 import UIKit
+import HelloJunaidSDK
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let label: UILabel = {
+            let label = UILabel()
+            label.frame.origin = CGPoint(x: 100, y: 100)
+            label.text = HelloJunaid().hello(to: "Junaid")
+            label.sizeToFit()
+            return label
+        }()
+        
+        self.view.addSubview(label)
     }
+  
 
 
 }
